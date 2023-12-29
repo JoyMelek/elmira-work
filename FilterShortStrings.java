14933e469a0b6a6727fcabdaa80a7d72aa3772e7
JoyMelek <ospanbekova_eg@jusan.kz>
2023-12-29 22:20:26 +0600

    - renamed the main class and added all the TODOs that must be done

---
+++ FilterShortStrings.java
@@ -0,0 +1,22 @@
+public class FilterShortStrings {
+    public static void main(String[] args) {
+        // TODO Example arrays
+
+        // TODO Processing and printing the results
+    }
+
+    public static String[] filterStrings(String[] input) {
+        // TODO Count short strings
+
+        // TODO Create a new array to hold short strings
+
+        // TODO Fill the new array
+
+        return null;
+    }
+
+    public static String arrayToString(String[] array) {
+        // TODO convert array to String
+        return "";
+    }
+}
